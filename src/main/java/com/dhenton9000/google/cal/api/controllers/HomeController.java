ab26c518d3c5e5bd44c4fa0bbe2f92ca601e9cb7
Don Henton <donhenton@gmail.com>
2016-12-10 17:32:15 -0600

    added demo showing that client context is session scoped.

--- src/main/java/com/dhenton9000/google/cal/api/controllers/HomeController.java
+++ src/main/java/com/dhenton9000/google/cal/api/controllers/HomeController.java
@@ -8,6 +8,7 @@ package com.dhenton9000.google.cal.api.controllers;
 import com.dhenton9000.google.cal.api.UserInfo;
 import java.security.Principal;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,22 +21,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 @Controller
 public class HomeController {
 
     @Value("${server.port}")
     private String serverPort;
-    
+
     @Value("${server.url}")
     private String serverUrl;
-    
+
     @Value("${spring.profiles.active}")
     private String activeEnv;
-    
+
+    @Autowired
+    ConfigurableWebApplicationContext applicationContext;
 
     private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
@@ -45,8 +52,7 @@ public class HomeController {
         return "pages/logout_done";
 
     }
-    
-    
+
     @RequestMapping("/graph")
     public String graph(Model model) {
         model.addAttribute("appTitle", "Graph");
@@ -63,27 +69,38 @@ public class HomeController {
         String className = principal.getClass().getName();
         LOG.info("XXXX "+className);
  
-    */
+     */
     @RequestMapping("/")
     public ModelAndView home(Principal principal, ModelAndView model) {
 
         Date d = new Date();
         
+        if (applicationContext != null) {
+            LOG.debug("start bean list");
+            //applicationContext.getBeanDefinitionNames();; used to dump
+            // bean names which can be used to lookup scope below
+
+            BeanDefinition def = applicationContext
+                    .getBeanFactory()
+                    .getBeanDefinition("scopedTarget.oauth2ClientContext");
+            
+            LOG.debug("\"scopedTarget.oauth2ClientContext\" " + def.getScope());
+        } else {
+            LOG.debug("app context is null");
+        }
         OAuth2Authentication auth = (OAuth2Authentication) principal;
 
         UserInfo userInfo = (UserInfo) auth.getUserAuthentication().getPrincipal();
-        String totalUrl = serverUrl+":"+ serverPort+" "+activeEnv;
+        String totalUrl = serverUrl + ":" + serverPort + " " + activeEnv;
         SimpleDateFormat sdfInput = new SimpleDateFormat("MM/dd/yyyy");
         String initialDate = sdfInput.format(d);
         model.addObject("appTitle", "Home Page");
-        model.addObject("initialDate",initialDate);
-        model.addObject("totalUrl",totalUrl);
-        model.addObject("userInfo",userInfo);
+        model.addObject("initialDate", initialDate);
+        model.addObject("totalUrl", totalUrl);
+        model.addObject("userInfo", userInfo);
         model.setViewName("pages/home");
         return model;
     }
-
-    
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
@@ -93,6 +110,5 @@ public class HomeController {
         }
         return "redirect:/logout_done";
     }
-
 
 }
